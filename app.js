fb523025c070897e78e1034359116d5a679ba48a
David Gibson <dayvidgibson@gmail.com>
2020-08-15 17:48:44 +0100

    add authors and stories

--- app.js
+++ app.js
@@ -33,6 +33,30 @@ app.get("/requests", function(req, res){
     res.render("requests");
 });
 
+app.get("/dickens", function(req, res){
+    res.render("authors/dickens");
+});
+
+app.get("/james", function(req, res){
+    res.render("authors/james");
+});
+
+app.get("/lefanu", function(req, res){
+    res.render("authors/lefanu");
+});
+
+app.get("/lovecraft", function(req, res){
+    res.render("authors/lovecraft");
+});
+
+app.get("/poe", function(req, res){
+    res.render("authors/poe");
+});
+
+app.get("/stoker", function(req, res){
+    res.render("authors/stoker");
+});
+
 app.listen(process.env.PORT || 3000, process.env.IP, function(){
     console.log("The horor anthology site is active.");
 });
