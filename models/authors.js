fb008453de41aa3545ad3f4b1e85a6b6a7144574
David Gibson <dayvidgibson@gmail.com>
2020-08-23 22:58:12 +0100

    small change to requests and file cleanup

--- models/authors.js
+++
@@ -1,9 +0,0 @@
-var mongoose = require("mongoose");
-
-// SCHEMA SETUP!
-var authorSchema = new mongoose.Schema({
-    name: String,
-    genre: String,
-});
-
-module.exports = mongoose.model("author", authorSchema);
